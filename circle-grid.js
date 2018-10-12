7bc0d307c6dcc30cb97a7e2a10358068ee05cac0
HermioneH <43250707+HermioneH@users.noreply.github.com>
2018-10-12 12:40:27 -0700

    Create circle-grid.js

---
+++ circle-grid.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+  for (var i = 10; i <= 400; i = i + 20) {
+	 for (var y = 10; y <=400; y = y + 20) {
+  
+		ellipse(i,y,10,10)
+	
+}
+}
+}
