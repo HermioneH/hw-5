857655a4c92fc48e5f7956e0b5d3695c347818a1
HermioneH <43250707+HermioneH@users.noreply.github.com>
2018-10-12 12:04:45 -0700

    Update diamond-lines.js

--- diamond-lines.js
+++ diamond-lines.js
@@ -6,5 +6,8 @@ function draw() {
   background(255);
   for (var i = 5; i <= 390; i = i + 10) {
     line(i+180,i, i-200,i);
+   for (var z = 5; z <= 390; z = z + 10) {
+    line(z,z+200, z,200-z);
   }
+}
 }
